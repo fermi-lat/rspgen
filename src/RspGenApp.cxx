5d3d978e4cb18ddb45a1e62e966746123272addf
peachey <>
2004-07-21 12:59:26 +0000

    Handle steady point source as well as burst case.

--- src/RspGenApp.cxx
+++ src/RspGenApp.cxx
@@ -4,11 +4,18 @@
 */
 #include <cstdlib>
 #include <memory>
+#include <stdexcept>
 
 #include "RspGenApp.h"
 #include "rspgen/GrbResponse.h"
+#include "rspgen/IResponse.h"
+#include "rspgen/PointResponse.h"
 
 namespace rspgen {
+
+  RspGenApp::RspGenApp(): m_bin_config(), m_data_dir(), m_response(0) {}
+
+  RspGenApp::~RspGenApp() throw() { delete m_response; }
 
   void RspGenApp::run() {
     st_app::AppParGroup & pars(getParGroup("rspgen"));
@@ -17,12 +24,28 @@ namespace rspgen {
   }
 
   void RspGenApp::prompt(st_app::AppParGroup & pars) {
+    // First determine which special case to handle.
+    pars.Prompt("respalg");
+    std::string alg = pars["respalg"];
+    if (alg == "GRB");
+    else if (alg == "PS");
+    else throw std::runtime_error("RspGenApp::prompt(): unknown/invalid response algorithm " + alg);
+    
     pars.Prompt("specfile");
     pars.Prompt("scfile");
     pars.Prompt("outfile");
     pars.Prompt("ra");
     pars.Prompt("dec");
-    pars.Prompt("time");
+
+    // In the burst case, the time of the burst is used to find a single RA/DEC.
+    if (alg == "GRB") pars.Prompt("time");
+    else {
+      // Non-burst case: SC data will be binned in theta space.
+      pars.Prompt("thetacut");
+      pars.Prompt("thetabinsize");
+    }
+
+    // Prompt for remaining parameters, which are common to all.
     pars.Prompt("psfradius");
     pars.Prompt("resptype");
     pars.Prompt("resptpl");
@@ -33,13 +56,9 @@ namespace rspgen {
     pars.Save();
   }
 
-  void RspGenApp::writeResponse(const st_app::AppParGroup & pars) const {
+  void RspGenApp::writeResponse(const st_app::AppParGroup & pars) {
     // Create energy binner from related parameters.
     std::auto_ptr<evtbin::Binner> true_en_binner(m_bin_config.createEnergyBinner(pars));
-
-    // Create response object.
-    GrbResponse response(pars["ra"], pars["dec"], pars["time"], pars["psfradius"], pars["resptype"], pars["specfile"],
-      pars["scfile"], true_en_binner.get());
 
     // Get name of template for output file.
     std::string resp_tpl = pars["resptpl"];
@@ -47,9 +66,25 @@ namespace rspgen {
     // If it was not defined, get default template file name.
     if (resp_tpl.empty()) resp_tpl = getDataDir() + "LatResponseTemplate";
 
-    // Write the output response file.
-    response.writeOutput("rspgen", pars["outfile"], resp_tpl);
+    // Determine which algorithm to use.
+    std::string alg = pars["respalg"];
 
+    // Clean up any previous response.
+    delete m_response; m_response = 0;
+
+    // Create response object.
+    if (alg == "GRB") {
+      m_response = new GrbResponse(pars["ra"], pars["dec"], pars["time"], pars["psfradius"],
+        pars["resptype"], pars["specfile"], pars["scfile"], true_en_binner.get());
+    } else if (alg == "PS") {
+      m_response = new PointResponse(pars["ra"], pars["dec"], pars["thetacut"], pars["thetabinsize"], pars["psfradius"],
+        pars["resptype"], pars["specfile"], pars["scfile"], true_en_binner.get());
+    } else {
+      throw std::runtime_error("RspGenApp::writeResponse: invalid response algorithm " + alg);
+    }
+
+    // Write the output response file.
+    m_response->writeOutput("rspgen", pars["outfile"], resp_tpl);
   }
 
   std::string RspGenApp::getDataDir() const {
