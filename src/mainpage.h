5fa1613d041204b605467fd0af5968079907b0a7
peachey <>
2004-07-21 16:38:38 +0000

    Remove explicit mention of latResponse.

--- src/mainpage.h
+++ src/mainpage.h
@@ -8,8 +8,7 @@
     This package consists of a class library and an application. The
     library contains abstractions which facilitate combining information
     from 1) the spacecraft pointing file, 2) a spectrum, and 3)
-    instrument response functions from the latResponse package to
-    produce an integrated, total instrument response in a format
-    acceptable to Xspec.
+    instrument response functions to produce an integrated, total instrument
+    response in a format acceptable to Xspec.
     
 */
